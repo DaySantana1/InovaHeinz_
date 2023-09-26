17e7a42b08db034276d5c0265e34935f5d0c2406
Dayane Santana <dayane.santana@dedalus.eu>
2023-09-26 15:58:32 -0300

    modificações e atualizações para a pagina de forms

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-$(document).ready(function(){
-    $('.carrossel').slick({
-        autoplay: true, // Iniciar a reprodução automática
-        autoplaySpeed: 1000, // Velocidade da reprodução automática (em milissegundos)
-        dots: true, // Mostrar indicadores de navegação (pontos)
-    });
-});
+const slider = document.querySelector(".slider");
+const value = document.querySelector(".value");
+
+value.textContent = slider.value;
+slider.oninput =function(){
+    value.textContent = this.value;
+}
+
